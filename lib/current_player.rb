06f9005ec4f9a2642fd3d9162f14dca73fdc45aa
Ariel Zhen <zhenpingping@live.com>
2018-05-18 01:45:25 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
 def turn_count(board)
   counter = 0
   board.each do |ele|
-    if |ele| == "X" || |ele| == "O"
+    if ele == "X" || ele == "O"
       counter += 1
     end
-  end 
+  end
   return counter 
 end
